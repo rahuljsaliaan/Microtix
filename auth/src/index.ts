3aba5ebb370f36e79144c4ea6f76e686fbd40c83
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-01-20 12:02:07 +0530

    Initial Commit with basic auth service and k8s config with skaffold

---
+++ auth/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import {currentUserRouter} from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+const app = express();
+app.use(express.json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.listen(3000, () => {
+  console.log('Auth service started on port 3000 🔐');
+});
