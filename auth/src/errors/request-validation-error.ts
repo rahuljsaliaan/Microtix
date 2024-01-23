6723666a358edfa592de89f9dbd68516f0a9323d
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-01-23 08:32:06 +0530

    Added: CustomError abstract class for handling known errors

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,22 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Invalid request parameters');
 
     // NOTE: Only because we are extending a built in class (only used in ts to solve transpilation issues in classes)
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((error) => {
+      if (error.type === 'field')
+        return { message: error.msg, field: error.path };
+
+      return { message: error.msg };
+    });
   }
 }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
@@ -13,6 +14,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use('*', () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
