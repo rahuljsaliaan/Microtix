8c129348ec80904ce5b8afc0b21d8b677393ba2c
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-01-27 23:18:10 +0530

    Added: tests for currentuser route 🧪🗺️

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+declare global {
+  var signup: () => Promise<string[]>;
+}
 
 let mongoServer: any;
 
@@ -24,3 +29,20 @@ afterAll(async () => {
   await mongoServer.stop();
   await mongoose.connection.close();
 });
+
+global.signup = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
