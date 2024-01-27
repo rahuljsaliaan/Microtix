9805190d6bae9a64d2de9003cb3a0fa952a1d7d6
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-01-27 12:39:53 +0530

    Added: currentuser route with authentication middlewares and error handling

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.post('/api/users/currentuser', (req: Request, res: Response) => {
-  res.send({});
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
