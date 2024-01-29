21f6c8d5fc94ab62925346250a86b4c8da73ebb2
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-01-30 00:37:03 +0530

    Updated: common package with desired middlewares and error handling code

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,11 +1,13 @@
-interface Color {
-  red: number;
-  blue: number;
-  green: number;
-}
+// Errors
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const color: Color = {
-  red: 10,
-  blue: 20,
-  green: 2,
-};
+// Middlewares
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
